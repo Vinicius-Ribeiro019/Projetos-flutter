4d5c4a8739dcde14ddb8fa14dcb397be1f6704ba
Vinicius <vinicius.s.ribeiro6@aluno.senai.br>
2026-03-31 11:14:49 -0300

    31/03/2026

--- lib/main.dart
+++ lib/main.dart
@@ -7,71 +7,26 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ContadorTela(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class ContadorTela extends StatefulWidget {
-  const ContadorTela({super.key});
-
-  @override
-  _CurtidasTelaState createState() => _CurtidasTelaState();
-}
-
-class _CurtidasTelaState extends State<ContadorTela> {
-  int curtidas = 0;
-
-  void incrementar() {
-    setState(() {
-      curtidas++;
-    });
-  }
-
-  void decrementar() {
-    setState(() {
-      if (curtidas > 0) {
-        curtidas--;
-      }
-    });
-  }
+  
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Aplicativo de Curtidas"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(Icons.favorite, color: Colors.red, size: 80),
-            const SizedBox(height: 10),
-            Text("$curtidas curtidas", style: const TextStyle(fontSize: 24)),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: incrementar,
-                  child: const Text("Curtir", style: TextStyle(color: Colors.black)),
-                ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: decrementar,
-                  child: const Text("Descurtir", style: TextStyle(color: Colors.black)),
-                ),
-              ],
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Texto e Imagem'),
         ),
-      ),
+        body:Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Bem vindo", style: TextStyle(fontSize: 24)),
+              const SizedBox(height: 20),
+              Image.asset('assets/images/image.png', width: 200),
+            ],
+          )
+          ),
+        )
     );
   }
 }
