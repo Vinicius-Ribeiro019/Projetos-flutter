9babd392ed63fa70bdeca95b1dc0770e32c3df04
Vinicius <vinicius.s.ribeiro6@aluno.senai.br>
2026-09-01 11:50:15 -0300

    Atividade GPS

--- lib/main.dart
+++ lib/main.dart
@@ -1,196 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MeuApp extends StatelessWidget {
-  const MeuApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Cartões Virtuais")),
-        body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: 20),
-                // Cartão Itaú - Laranja
-                Container(
-                  width: 350,
-                  height: 200,
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Colors.orange,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Itaú",
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Icon(
-                            Icons.contactless,
-                            size: 25,
-                            color: Colors.white,
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(height: 20),
-                          Icon(Icons.sim_card, size: 25, color: Colors.blue),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            "4567 8901 2345 6789",
-                            style: TextStyle(fontSize: 25, color: Colors.white),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Titular",
-                            style: TextStyle(fontSize: 12, color: Colors.white),
-                          ),
-                          Text(
-                            "Validade",
-                            style: TextStyle(fontSize: 12, color: Colors.white),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Yago de Lima Pavan",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            "05/28",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 30),
-                // Cartão Nubank - Roxo
-                Container(
-                  width: 350,
-                  height: 200,
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Colors.deepPurple,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Nubank",
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Icon(
-                            Icons.contactless,
-                            size: 25,
-                            color: Colors.white,
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(height: 20),
-                          Icon(
-                            Icons.sim_card,
-                            size: 25,
-                            color: Colors.yellowAccent,
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            "1895 1634 5674 2342",
-                            style: TextStyle(fontSize: 25, color: Colors.white),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Titular",
-                            style: TextStyle(fontSize: 12, color: Colors.white),
-                          ),
-                          Text(
-                            "Validade",
-                            style: TextStyle(fontSize: 12, color: Colors.white),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Yago de Lima Pavan",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            "10/31",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 20),
-              ],
+      debugShowCheckedModeBanner: false,
+      title: 'minha localização',
+      home: const LocalizacaoPage(),
+    );
+  }
+}
+
+class LocalizacaoPage extends StatefulWidget {
+  const LocalizacaoPage({super.key});
+
+  @override
+  State<LocalizacaoPage> createState() => _LocalizacaoPageState();
+}
+
+class _LocalizacaoPageState extends State<LocalizacaoPage> {
+  double latitude = 0;
+  double longitude = 0;
+
+  Future<void> buscarLocalizacao() async {
+    bool servicoAtivo = await Geolocator.isLocationServiceEnabled();
+
+    if (!servicoAtivo) {
+      await Geolocator.openLocationSettings();
+      return;
+    }
+
+    LocationPermission permissao = await Geolocator.checkPermission();
+
+    if (permissao == LocationPermission.denied) {
+      permissao = await Geolocator.requestPermission();
+    }
+
+    if (permissao == LocationPermission.denied ||
+    permissao == LocationPermission.deniedForever) {
+      return;
+    }
+
+    Position posicao = await Geolocator.getCurrentPosition();
+
+    setState(() {
+      latitude = posicao.latitude;
+      longitude = posicao.longitude;
+    });
+
+    print('Latitude: $latitude');
+    print('Longitude: $longitude');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Minha Localização')),
+
+      body: Center(
+       child:Padding(
+         padding: const EdgeInsets.all(20),
+
+         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+
+          children: [
+            const Icon(Icons.location_on, size: 80, color: Colors.red),
+
+            const SizedBox(height: 20),
+
+            const Text(
+              'Localização Atual',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-          ),
+
+            const SizedBox(height: 30),
+
+            Text('Latitude: $latitude', style: const TextStyle(fontSize: 18)),
+
+            const SizedBox(height: 10),
+
+            Text(
+              'Longitude: $longitude',
+              style: const TextStyle(fontSize: 18),
+            ),
+
+            const SizedBox(height: 30),
+
+            ElevatedButton(
+              onPressed: buscarLocalizacao,
+              child: const Text('Buscar Localização'),
+            )
+          ],
+         ),
         ),
       ),
     );
